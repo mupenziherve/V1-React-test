e068cbb0362f36298ebc2d862a1abdfe9120e924
Mupenzi Herve <mupenziherve04@gmail.com>
2025-06-26 14:14:49 +0000

    useState Object

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,24 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [name, setName] = useState('peter');
+  const [age, setAge] = useState(22);
+  const [hobby, setHobby] = useState('Playing Basketball');
+
+  const displayPerson = () => {
+    setName('mupenzi');
+    setAge(70);
+    setHobby('I like coding');
+
+  }
+
+  return <>
+  
+  <h4><h2>Name:</h2>{name}</h4>;
+  <h4><h2>Age:</h2>{age}</h4>;
+  <h4><h2>Hobby:</h2>{hobby}</h4>;
+  <button className='btn' onClick={displayPerson}>Show Mupenzi</button>
+  </>
 };
 
 export default UseStateObject;
