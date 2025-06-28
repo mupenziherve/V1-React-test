2358bc3eda54353205a069e10e0c0e7a69bbda21
Mupenzi Herve <mupenziherve04@gmail.com>
2025-06-28 13:45:03 +0200

    function approach

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -4,9 +4,15 @@ const UseStateGotcha = () => {
   const [value, setValue] = useState(0)
 
   const handleClick = () =>{
-    setValue(value + 1);
-    console.log()
-  }
+    setTimeout(() => { 
+      console.log('clicked the button');
+      setValue((currentState) => {
+        
+      })
+      setValue(value + 1);
+       },3000);
+  
+  };
   return <div><h2>{value}</h2>
   <button type="button" className="btn" onClick={handleClick}>Increase</button>
   </div>
