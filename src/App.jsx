ded3d73fde10855c8a95b4bc194fcb82db16adde
Mupenzi Herve <mupenziherve04@gmail.com>
2025-06-28 23:14:58 +0200

    remove test

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className='container'>
       <Starter/>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo neque dolore earum doloribus obcaecati consequuntur iure optio laborum autem officia id inventore, quisquam minus delectus, qui velit porro, eos reiciendis?</h1>
+     
     </div>
   );
 }
